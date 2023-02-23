b804cfa16a889f42a64f887f8dd7cf7db7291391
juanan fernandez <juanan.fernandez@gmail.com>
2023-02-23 21:05:56 +0100

    add javascript router and home page

--- js/game.js
+++ js/game.js
@@ -16,3 +16,9 @@ export const getWinner = (player1Option, player2Option) => {
 	// Si no hay empate ni ganador del jugador 1, el jugador 2 es el ganador
 	return 2;
 };
+
+const getComputerOption = () => {
+	const options = ['rock', 'paper', 'scissors'];
+	const randomNumber = Math.floor(Math.random() * 3);
+	return options[randomNumber];
+};
