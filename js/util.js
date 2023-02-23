b210f7d98759a8b5310ee9bb1324cc2ec7118e44
juanan fernandez <juanan.fernandez@gmail.com>
2023-02-23 20:19:40 +0100

    first commit

---
+++ js/util.js
@@ -0,0 +1,18 @@
+export const getWinner = (player1Option, player2Option) => {
+	//empate?
+	if (player1Option === player2Option) {
+		return 0;
+	}
+
+	// Comprobamos las condiciones para el jugador 1 ganar
+	if (
+		(player1Option === 'piedra' && player2Option === 'tijeras') ||
+		(player1Option === 'papel' && player2Option === 'piedra') ||
+		(player1Option === 'tijeras' && player2Option === 'papel')
+	) {
+		return '1';
+	}
+
+	// Si no hay empate ni ganador del jugador 1, el jugador 2 es el ganador
+	return 2;
+};
